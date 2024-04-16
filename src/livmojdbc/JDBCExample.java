2d7e1b18c4ca7c095f8a2ccdbf4f13c80611d0e6
abirami0123 <147804064+abirami0123@users.noreply.github.com>
2024-04-16 12:33:04 +0530

    my second try commit

---
+++ src/livmojdbc/JDBCExample.java
@@ -0,0 +1,23 @@
+package livmojdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class JDBCExample {
+	public static void main(String args[]) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "Abi@dbms"); 
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from employee");
+			System.out.println("testing log");
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
