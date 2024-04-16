cd03cfa2b70848a659bcb2ef3bf08b65e7121bf2
abirami0123 <147804064+abirami0123@users.noreply.github.com>
2024-04-16 14:00:09 +0530

    page factory changes

--- src/livmojdbc/JDBCExample.java
+++ src/livmojdbc/JDBCExample.java
@@ -12,7 +12,7 @@ class JDBCExample {
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "Abi@dbms"); 
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from employee");
-			System.out.println("testing log");
+			System.out.println("testing log change");
 			while (rs.next())
 				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
 			con.close();
